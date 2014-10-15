08b1763bd1e3670c2f19fe6373a38a80f6b47a3d
smitbmx <smitbmx@gmail.com>
2014-10-15 20:51:17 +0300

    Added new structURAL pattern Adapter

---
+++ Adapter/ElectricityConsumer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class ElectricityConsumer
+    {
+        public static void ChargeNotebook(INewElectricitySystem electricitySystem)
+        {
+            Console.WriteLine(electricitySystem.MatchWideSocket());
+        }
+    }
+}
