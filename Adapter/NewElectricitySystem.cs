03ad395f83b3d65bf90b525eafe204f50cf8650d
smitbmx <smitbmx@gmail.com>
2014-10-15 20:51:58 +0300

    Revert "Revert "Added new structURAL pattern Adapter""
    This reverts commit c4c6c16f747b7dd16a939fc3b427cccc20ea1b7f.

---
+++ Adapter/NewElectricitySystem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class NewElectricitySystem : INewElectricitySystem
+    {
+        public string MatchWideSocket()
+        {
+            return "220V";
+        }
+    }
+}
